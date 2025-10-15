a8db0382145efe1358b81c755f622a2b193fe12b
Adarsh <sunny.yadav.engr@gmail.com>
2025-10-15 12:56:02 +0530

    Updated here section

--- src/data/personal-info.js
+++ src/data/personal-info.js
@@ -1,9 +1,9 @@
 export const personalInfo = {
   name: "Adarsh Yadav",
   nickname: "Sunny",
-  title: "Full Stack Developer",
+  title: "Software Engineer",
   email: "adarsh.yadav.dev@gmail.com",
-  phone: "+91-XXXXXXXXXX",
+  phone: "+91-8840820604",
   location: "Kanpur, Uttar Pradesh, India",
   bio: "Passionate full-stack developer with expertise in modern web technologies. I love creating scalable applications and solving complex problems.",
   avatar: "/images/profile/avatar.jpg",
