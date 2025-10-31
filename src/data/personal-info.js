6f4a29bf8b723b4e165ae41fb115dce0f9473340
Adarsh <sunny.yadav.engr@gmail.com>
2025-10-31 17:41:22 +0530

    Updated skill  page

--- src/data/personal-info.js
+++ src/data/personal-info.js
@@ -6,7 +6,7 @@ export const personalInfo = {
   phone: "+91-8840820604",
   location: "Kanpur, Uttar Pradesh, India",
   bio: "Passionate full-stack developer with expertise in modern web technologies. I love creating scalable applications and solving complex problems.",
-  avatar: "/images/profile/avatar.jpg",
+  avatar: "/images/profile/profile-picture.jpg",
   resume: "/documents/resume.pdf",
   yearsOfExperience: 2
 }
